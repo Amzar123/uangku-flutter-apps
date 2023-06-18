bce7c607824025841340150ca7d88cc495e7b2ab
HanifahAH01 <94789593+HanifahAH01@users.noreply.github.com>
2023-06-18 15:06:23 +0700

    loginpage.dart

--- lib/src/features/auth/loginpage.dart
+++ lib/src/features/auth/loginpage.dart
@@ -1,51 +1,196 @@
-jkshksajdk;AHIAKSM,XSAZXOIAKLSMX,zXNIUWSNX,ZXHAKLKS.,XBLA;SXMN,XNAKSLSxaS
-  AHJXSZ,.XMWOIASKNX,hSOLm
-zSJGIAKSNXIAKLSA,Z.XA
-  ASJBMZGAJKLSJKKA,.S
+import 'package:flutter/material.dart';
+import 'package:apps/src/features/auth/registerpage2.dart';
 
-  ASJASK,.AJAWSKYE,ZX
-  ASGAISMNAN,ASHUQWJAMNXLkysWA
-  WHSDJASKJLa,bakasd
-  sabdajsjjaklwkaxkadhuwa
-  jdsguwndkajqywejm,msa
-  sajkamdmSYuqwemnasghw
-  bhsbdsaKJAEYUWM,SDA,IWE
-  HSDAJEBNSDAKEW
-  HSDAJEQWYUJBSDUKWEMAS
-  BHJSDBJHASGUKSAL
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
-  DSJKHDASUJEBWMASWAS
-  BSBDUIWEQJNMADS
-  BSDAJHWEJNMSDGHUIWEKJNAMDSGWKJDSA
-  NBSDUIQWJBMASLALGUWEJSMN,ZMD;AIEIUJWNM,DAS
-    JJDSKLAEUUWIEIIPWYRUILOWIJSNAMAKAOLIYEWIOIAS
-    SNDHUIAWMSNDJLAYEILAKSORJLKEOQJELQWW
-    NDLKJDWPOEKIUREWKSHEWKLFESL[
-    SJKJDFJOIESNSM,ZLSIEJKCLJFJNSE
-    EHIFSK,MCNKHKSDHFSEOEIORLSM,DFIKS,
-    DJNKSLFPWEUIWEOJNSCM,OEUHESK,KSKDPWEERI3GRJKSHFLSOEIURIEKG;LEWUIUTKWLOTYOUHGKFSFKX,VNNISKENEKSJMZHISDFNFXMVBOENVD
-    JFJNKSDHXNKDMFNISOFBBVKJFEFIIFEWKJF ,XJKCKEFJSKNCKEFSIDUFMNXCHUEMNDSOENMESOEW
-    JDFJSKJEKNSDKEUWUEWSDKLNJIUKSJJDISS
-    A
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(),
+                  margin: EdgeInsets.only(bottom: 30, top: 20),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(),
+                          Container(
+                            margin:
+                            EdgeInsets.only(bottom: 0, top: 20),
+                            child: const Image(
+                                image: AssetImage('assets/TemanInvest_Logo.png')),
+                            width: 250,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  margin: EdgeInsets.only(bottom: 30, top: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Email"),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            contentPadding:
+                            EdgeInsets.symmetric(vertical: 16.0),
+                            labelText: "Email",
+                            prefixIcon: Icon(Icons.email_outlined),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Password"),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: TextField(
+                          obscureText: true,
+                          enableSuggestions: false,
+                          autocorrect: false,
+                          style:
+                          TextStyle(fontSize: 16.0, color: Colors.black),
+                          decoration: InputDecoration(
+                            contentPadding:
+                            EdgeInsets.symmetric(vertical: 16.0),
+                            labelText: "Password",
+                            prefixIcon: Icon(Icons.lock_outlined),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(50)),
+                              borderSide: BorderSide(
+                                color: Color(0xFF5C62FF),
+                                width: 3,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 5),
+                  height: 30,
+                  width: 200,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => Home(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      "Masuk",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0.0,
+                      primary: Color(0xFF57C5B6),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Belum memiliki akun?",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => RegisterPage2(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        ' Daftar sekarang',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-    HSDAUYUWQIEU9ITRUEWLIOQPWUOWYRMXBCZKYDKSNZCXHDHKSMCIHESKRJ3WEUTIRTLPOWWERSBZNLASUIWEJDSJMDSHWIEDNSM
-    UIDUDSMNDSUUEWNNSSM,SUIDWBNSKSDLAUDNMSDLKSDEUWRJKSDJLISD
-    'HJDUDJNMDSAJIJIEWMNJKDUIIU
-    JKDSIEUWJEWN,SDJHJDJDDS'HWKLDHWHUUIEHJSNMSKMDOIUEBNNJDK"zJIIFHUEWJNSDOIYUERHJLKSAOIYUREWJNJSKSUIHSDDKSIUGFEHKSDIUAYUFJESDKLSDAGEENDJLKSDAUIHJ
-    SDHUUDNJDASJKUEFUKLJSDAJKLHUSDELKESD"
-JKSDYIUWJENAJLUYGREUJSKLKWDIYURKJSKJIWUEHIEWUALSGYUJSDJHUEWIKEKOPWKGFJHCNMXNMOAIYHRNDOALAYEHWUJKAS
-    DSH7WUKAEJDMNXB7IEUWJKGSHUZKIDWBOEIKYRHBOQWEHFIWUEKOWBF I VUI WKV OEWOI EL ELIWIBE W EOIKSDUIYUWEKJSHHIU IUWEESKZXNXSA'
-    SIDHOXI  WUE AKS,H,SYYUIKESDZHDA'GASDIUAWE DMNXZIUWEAJKSMZXNUIWUIET7OWEIUYERDHSMZXUYUEEYUEHSDBJASD
-    SHUIDSUJHEWNJGHDSWWEYUWUIDJSSKOIPSDYUJKREKNDSAHUIEWNM SDAJKASEHDNMZX SXZLKHDDWJEKM,SAXNAUMS XASIYEHDNSM ZXHUAWJDSMZ XAISDHJXNM FDKRYHEFND C
-    DBSJF7HUSHJN CUIWEJKSBN CUIDGRHJNMC,
-    DJASITUGJNMEW SDXZIOEKWLSD
-    ZWJDSAGUYZHNESDIXZUJKEMW,ASD
-    Z NSDBC7UIJEWNSD XZUIJKWEAS
-    JBSGSUIDZXHJBN AMMS,.LIRUIYEWUISOWHJSDNM,FLIUYTEUIOPWIUYDGHSMXCN VBCGHFYUEIWOSKALMXBNCHGYSDUFEUIOWLKDS,C.S
-    'JDHSUYDIEQWOKJSDSGHFYUISKJDCBVCNXM,ZAXSKLIOWEOUIHDSJK
-    NSHJDUYEWJNDBFGYRUEWIJSKA'
-    SAJYUAWSIJKXMKLDSIUYUEWKDLIOUCUHSJ
-    JHDYUFRGEWKSLIUIYHXKLUYGFVBNMXKLUIYGHN
-    SDHYT78UIWEJKUYRGFHBNDSMKALIODUEYUWHNDSM,
-    XNSHJYUSDGHBENMKDSIOUYTYGFVBNMXZKLSIODEW
-    USYXDGHWABNMK XCXJKCB SAXBUSAJ X
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Home Page!'),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(title: 'Login'),
+    );
+  }
+}
